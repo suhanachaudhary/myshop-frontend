cac095a914c8e0811259f7e69af6ff20e175888b
CHAUDHARY_SUHANA <suhanachaudhary212@gmail.com>
2026-01-13 18:24:19 +0530

    price added1

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -48,7 +48,7 @@ const Checkout = () => {
                     ))}
                 </div>
 
-                <div className="grid grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
 
                     <div className="col-span-1 md:col-span-2 space-y-4">
 
